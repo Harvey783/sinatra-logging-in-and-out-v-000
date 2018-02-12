8fba1ae04abd06c259ba9b99d2512237194eac1c
Chris Harvey <chharvey@icloud.com>
2018-02-12 02:44:32 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,10 @@
 class Helpers
-  def self.current_user(session)
-    User.find(session[:user_id])
+  def self.current_user(session_hash)
+      User.find(session_hash[:user_id], session_hash[:password])
   end
 
-  def self.is_logged_in?(session)
-    !!session[:user_id]
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
   end
+
 end
